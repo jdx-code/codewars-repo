446a17d7b1587c4a3f35df02916b8ff99cf3d89b
Jyotirmoy <jyotirmoydas12@gmail.com>
2022-12-07 19:31:43 +0530

    Kata completed

---
+++ 7kyu/numToRevArrayOfDigits.js
@@ -0,0 +1,21 @@
+// Convert number to reversed array of digits
+
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+// Example(Input => Output):
+
+// 35231 => [1,3,2,5,3]
+// 0 => [0]
+
+function digitize(n){
+    // Using simple while loop and push function
+    var revArr = [];
+    var rem;
+    while(n!=0){
+        rem = n%10;
+        revArr.push(rem);
+        n = Math.floor(n/10);
+    }
+    return revArr;
+}
+
+console.log(digitize(12345));
