5ea293c7ea1e77c639397e423014d1f61102073c
Jyotirmoy <jyotirmoydas12@gmail.com>
2022-12-14 22:01:15 +0530

    Kata completed

--- 7kyu/shortestWord.js
+++ 7kyu/shortestWord.js
@@ -2,17 +2,36 @@
 // String will never be empty and you do not need to account for different data types.
 
 
-function findShort(s){
+function findShort(s){   
 
-    // Solution #1
-    let arrOfWords, sortedArr = [];    
-    arrOfWords = s.split(" ");
-    sortedArr = arrOfWords.sort((a, b) => a.length - b.length);
-    return sortedArr[0].length;
-
-    // Solution #2
-    // sortedArr = arrOfWords.map(e=>e).sort((a,b) => a.length - b.length);
+    // Method #1 : Using sort function
+    // let arrOfWords, sortedArr = [];    
+    // arrOfWords = s.split(" ");
+    // sortedArr = arrOfWords.sort((a, b) => a.length - b.length);
     // return sortedArr[0].length;
+
+    // Method #2 : Using map function
+    // let arrOfWords = [];
+    // arrOfWords = s.split(' ');
+    // let minLen = 999;
+    // arrOfWords.map((word) => {        
+    //     if(minLen > word.length){
+    //        minLen = word.length;
+    //     }        
+    // });
+    // return minLen; 
+    
+    // Method #3 : Using forEach 
+    let arrOfWords = [];
+    arrOfWords = s.split(' ');
+    let minLength = 999;
+    arrOfWords.forEach(element => {
+        if(minLength > element.length){
+            minLength = element.length
+        }
+    });
+    return minLength;
+
 }
 
 console.log(findShort("Monday could be the day."))
