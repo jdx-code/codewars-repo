db28661b746072f478d1bd9c1b2188d7dbebc842
Jyotirmoy <jyotirmoydas12@gmail.com>
2022-12-16 00:09:45 +0530

    Kata Completed

--- 8kyu/theFeastOfManyBeasts.js
+++ 8kyu/theFeastOfManyBeasts.js
@@ -4,8 +4,14 @@
 
 // Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
 
-
 function feast(beast, dish){
+
+    // Method #4 : Using slice function
+    return beast.slice(0,1) === dish.slice(0,1) && beast.slice(-1) === dish.slice(-1) ? true : false;
+
+    // Method #3 : Using shift/pop functions
+    // return beast.split('').shift() === dish.split('').shift() && beast.split('').pop() === dish.split('').pop() ? true : false;    
+    
     // let beastArr = beast.split('');
     // let dishArr = dish.split('');
     // let firstLetterOfBeast = beastArr[0];    
@@ -13,18 +19,15 @@ function feast(beast, dish){
     // let firstLetterOfDish = dishArr[0];
     // let lastLetterOfDish = dishArr[dishArr.length-1];
 
+    // Method #2 : Using ternary operator
+    // return firstLetterOfBeast === firstLetterOfDish && lastLetterOfBeast === lastLetterOfDish ? true : false;
+
     // Method #1 : Using simple if-else
     // if(firstLetterOfBeast === firstLetterOfDish && lastLetterOfBeast === lastLetterOfDish){
     //     return true;
     // } else {
     //     return false;
-    // }
-
-    // Method #2 : Using ternary operator
-    // return firstLetterOfBeast === firstLetterOfDish && lastLetterOfBeast === lastLetterOfDish ? true : false;
-
-    // Method #3 : Using shift/pop operators
-    return beast.split('').shift() === dish.split('').shift() && beast.split('').pop() === dish.split('').pop() ? true : false;
+    // }    
 }
 
-console.log(feast("chickadee", "chocolate-cakes"));
+console.log(feast("chickadee", "chocolate-cake"));
