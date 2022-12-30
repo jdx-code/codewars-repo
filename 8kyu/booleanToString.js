32790363c82e4be2d706211ab56e614010dff697
Jyotirmoy <jyotirmoydas12@gmail.com>
2022-12-30 22:01:48 +0530

    23 katas completed

---
+++ 8kyu/booleanToString.js
@@ -0,0 +1,9 @@
+// Kata # 23 of 8kyu
+// Implement a function which convert the given boolean value into its string representation.
+// Note: Only valid inputs will be given.
+
+const booleanToString = b => {
+    return b.toString();
+}
+
+console.log(booleanToString(false));
