5a8bbea3da2ba5fc08ef7d312d09ba740e77765c
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-01-02 20:07:27 +0530

    25 katas completed

---
+++ 8kyu/simpleMultiplication.js
@@ -0,0 +1,8 @@
+// Kata # 25 of 8kyu
+//This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+const simpleMultiplication = (number) => {
+    return number%2 === 0 ? number * 8 : number * 9;
+}
+
+console.log(simpleMultiplication(4));
