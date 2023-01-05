0a9fc821cd7d11e3d9e2ddd2e83c168ce382c517
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-01-05 22:28:51 +0530

    17 katas completed

---
+++ 7kyu/testingOneTwoThree.js
@@ -0,0 +1,13 @@
+// Kata # 17 of 7kyu
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+// Examples: (Input --> Output)
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+const testingOneTwoThree = (arr) => {
+    // Method #1 : Using map function without template literals
+    return arr.map((ele, i) => i+1 +': '+ ele)
+}
+console.log(testingOneTwoThree(["a", "b", "c"]));
