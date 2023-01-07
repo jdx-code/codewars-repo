0ad4484900f60b428ecd531ad21b20eb1a63057d
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-01-07 21:33:32 +0530

    29 katas completed

---
+++ 8kyu/sumMixedArray.js
@@ -0,0 +1,10 @@
+// Kata # 29 of 8kyu
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+// Return your answer as a number.
+
+const sumMix = (x) => {    
+    // Method #1: Using map(Number)
+    return x.map(Number).reduce((acc, ele) => acc + ele, 0);
+}
+
+console.log(sumMix([9, 3, '7', '3']));
