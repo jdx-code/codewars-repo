a9b817ec3c2e288c7f1ab9c0ec513804d3a2b96f
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-01-10 20:07:26 +0530

    33 katas completed

---
+++ 8kyu/sumOfPositive.js
@@ -0,0 +1,14 @@
+// Kata # 33 of 8kyu
+// Description:
+// You get an array of numbers, return the sum of all of the positives ones.
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+// Note: if there is nothing to sum, the sum is default to 0.
+
+const sumOfPositives = (arr) => {
+    
+    // Method #1 : Using reduce with filter 
+    return arr.filter(item=>item>0).reduce((sum, item) => sum + item, 0);  
+
+}
+
+console.log(sumOfPositives([1,-4,7,12]));
