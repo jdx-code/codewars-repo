575b5bd2d75e5b3db44fe4f1334338353eb65b0c
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-01-12 22:03:49 +0530

    34 katas completed

--- 8kyu/calculateBMI.js
+++ 8kyu/calculateBMI.js
@@ -8,15 +8,22 @@
 
 const bmiFunction = (weight, height) => {
     const bmi = weight/(height*height);    
-    if(bmi<=18.5){
-        return "Underweight";
-    } else if(bmi<=25.0){
-        return "Normal"
-    } else if(bmi<=30.0){
-        return "Overweight";
-    } else {
-        return "Obese";
-    }
+
+    // Method #1 : Using if-else
+    // if(bmi<=18.5){
+    //     return "Underweight";
+    // } else if(bmi<=25.0){
+    //     return "Normal"
+    // } else if(bmi<=30.0){
+    //     return "Overweight";
+    // } else {
+    //     return "Obese";
+    // }
+
+    // Method #2 : Using ternary operator
+    return bmi <= 18.5 ? "Underweight" :
+            bmi <= 25.0 ? "Normal" :
+            bmi <= 30.0 ? "Overweight" : "Obese";
 }
 
 console.log(bmiFunction(80, 1.80));
