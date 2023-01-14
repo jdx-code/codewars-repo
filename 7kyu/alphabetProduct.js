4316906c89e55b9e55f842f0a88085ca32978b2c
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-01-14 20:54:31 +0530

    20 katas completed

---
+++ 7kyu/alphabetProduct.js
@@ -0,0 +1,19 @@
+// Kata # 22 of 7kyu
+// I have four positive integers, A, B, C and D, where A < B < C < D. The input is a list of the integers A, B, C, D, AxB, BxC, CxD, DxA in some order. Your task is to return the value of D.
+
+const alphabetProduct = (arr) => {   
+   let uniqueItems = [];
+   for(let item of arr){
+    if(!uniqueItems.includes(item)){
+        uniqueItems.push(item);
+    }
+   }
+   return uniqueItems.sort((a,b)=>a-b)[3];
+}
+
+// console.log(alphabetProduct([2,3,4,1,12,6,2,4]));
+// console.log(alphabetProduct([2,6,7,3,14,35,15,5]));
+// console.log(alphabetProduct([20,10,6,5,4,3,2,12]));
+// console.log(alphabetProduct([2,6,18,3,6,7,42,14]));
+// console.log(alphabetProduct([7,96,8,240,12,140,20,56]));
+console.log(alphabetProduct([20,30,6,7,4,42,28,5]));
---
+++ 7kyu/countConsonants.js
@@ -0,0 +1,20 @@
+// Kata # 20 of 7kyu
+// Complete the function that takes a string of English-language text and returns the number of consonants in the string.
+// Consonants are all letters used to write English excluding the vowels a, e, i, o, u.
+
+const countConsonants = (str) => {    
+    // Solution unlocked  
+    // Useful link : https://bobbyhadz.com/blog/javascript-cannot-read-property-length-of-null
+    return (str.match(/[bcdfghjklmnpqrstvwxyz]/ig) || [] ).length;    
+}
+
+// console.log(countConsonants(''));
+// console.log(countConsonants('aaaaa'));
+// console.log(countConsonants('XaeiouX'));
+// console.log(countConsonants('Bbbbb'));
+// console.log(countConsonants('helLo world'));
+// console.log(countConsonants('h^$&^#$&^elLo world'));
+// console.log(countConsonants('0123456789'));
+// console.log(countConsonants('012345_Cb'));
+console.log(countConsonants('uooeeeiioiioaeuiaeiaaaeouuoeueauuuufrkfkstjndklzmhtfg'));
+
