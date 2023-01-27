2aa37a6e299eadd035b484afe783376a937c1091
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-01-27 19:48:09 +0530

    38 katas completed

--- 8kyu/stringToNumber.js
+++ 8kyu/stringToNumber.js
@@ -1,4 +1,4 @@
-
+// Kata # 38 of 8kyu
 // Description
 // We need a function that can transform a string into a number. What ways of achieving this do you know?
 // Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
@@ -7,3 +7,17 @@
 // "605"  --> 605
 // "1405" --> 1405
 // "-7" --> -7
+
+const stringToNumber = (str) => {
+    // Method #1: Solution by me
+    // return Number(str);
+
+    // Method #2: Solution found on Codewars
+    // return parseInt(str);
+
+    // Method #3: Solution found on Codewars
+    return +str;
+}
+
+console.log(stringToNumber("1234"));
+console.log(stringToNumber("-7"));
