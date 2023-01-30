1ed8cbe4cd86598674ca5f3671b25a3a716d30fd
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-01-30 22:59:09 +0530

    40 katas completed

--- 8kyu/countPositivesSumOfNegatives.js
+++ 8kyu/countPositivesSumOfNegatives.js
@@ -6,16 +6,17 @@
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
 const countPositivesSumOfNegatives = (arr) => {    
+    // Method #1 : My solution
     let result = [];    
     if(arr && arr.length){    // have to check if it's an array and if the array contains any element or not
-        const countOfPositives = (arr.filter(n => n > 0) || [+0]).length;
+        const countOfPositives = arr.filter(n => n > 0).length;
         result.push(countOfPositives);
-        const sumOfNegatives = (arr.filter(n => n < 0) || [+0]).reduce((acc, n) => acc + n, 0)
+        const sumOfNegatives = arr.filter(n => n < 0).reduce((acc, n) => acc + n, 0)
         result.push(sumOfNegatives);
         return result;      
     } else {
         return result;
-    }  
+    }     
 }
 
 console.log(countPositivesSumOfNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));
