972fd949f625d6b30adcda8693a01e664eafa292
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-01-31 20:16:43 +0530

    6 katas completed

---
+++ 6kyu/expandedForm.js
@@ -0,0 +1,21 @@
+// Kata # 6 of 6kyu
+// Write Number in Expanded Form
+// You will be given a number and you will need to return it as a string in Expanded Form. For example:
+// expandedForm(12); // Should return '10 + 2'
+// expandedForm(42); // Should return '40 + 2'
+// expandedForm(70304); // Should return '70000 + 300 + 4'
+// NOTE: All numbers will be whole numbers greater than 0.
+
+const expandedForm = (num) => {
+    return num.toString()
+                .split('')
+                .reverse()
+                .map((item, index) => item * Math.pow(10, index))
+                .filter(num => num > 0)
+                .reverse()
+                .join(' + ');
+}
+
+console.log(expandedForm(12));
+console.log(expandedForm(42));
+console.log(expandedForm(70304));       
