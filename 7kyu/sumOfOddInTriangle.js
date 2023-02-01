950f9d91fcfa112734f3fdf27f7cb9a0aed05746
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-02-01 21:54:26 +0530

    27 katas completed

---
+++ 7kyu/sumOfOddInTriangle.js
@@ -0,0 +1,22 @@
+// Kata # 27 of 7kyu
+// Given the triangle of consecutive odd numbers:
+
+//              1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+// ...
+
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+// 1 -->  1
+// 2 --> 3 + 5 = 8
+
+const rowSumOddNumbers = (num) => {
+    // Method #1 : Solution found on Codewars
+    return n * n * n;
+}
+
+console.log(rowSumOddNumbers(1));
+console.log(rowSumOddNumbers(2));
