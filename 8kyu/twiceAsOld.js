e04eeb8d0f153f5f954099d91aff6c14f070601a
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-02-02 20:43:51 +0530

    43 katas completed

---
+++ 8kyu/twiceAsOld.js
@@ -0,0 +1,17 @@
+// Kata # 43 of 8kyu
+// Description:
+
+// Your function takes two arguments:
+
+//     current father's age (years)
+//     current age of his son (years)
+
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+
+const twiceAsOld = (dadYearsOld, sonYearsOld) => {
+    // Method #1 : Solution by me
+    return Math.abs(dadYearsOld - 2*sonYearsOld);
+}
+
+console.log(twiceAsOld(36, 7));
+console.log(twiceAsOld(55, 30));
