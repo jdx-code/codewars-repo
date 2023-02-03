6a0c5a9a6789255112f2b7fb806a1a66eaf1976f
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-02-03 19:17:18 +0530

    44 katas completed

---
+++ 8kyu/evenOrOdd.js
@@ -0,0 +1,7 @@
+// Kata # 44 of 8kyu
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+const evenOrOdd = num => num%2 === 0 ? "Even" : "Odd";
+
+console.log(evenOrOdd(2));
+console.log(evenOrOdd(7));
