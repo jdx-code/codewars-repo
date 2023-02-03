9c39e6db3b04b7e89da5363294636d88eca8da08
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-02-03 19:38:06 +0530

    28 katas completed

---
+++ 7kyu/binaryAddition.js
@@ -0,0 +1,25 @@
+// Kata # 28 of 7kyu
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+// The binary number returned should be a string.
+
+// Examples:(Input1, Input2 --> Output (explanation)))
+
+// 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+// 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+
+const binaryAddition = (a, b) => {
+    // Method #1 : Solution by me
+    let sum = a + b;
+    let binarySum = [];
+    while (sum>0){
+        let rem = sum%2;
+        if(rem === 0 || rem === 1){
+            binarySum.push(rem);
+        }        
+        sum = Math.floor(sum/2);
+    }
+    return binarySum.reverse().join('');
+}
+
+console.log(binaryAddition(5, 9));
