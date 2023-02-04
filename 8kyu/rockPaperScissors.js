bc9f5a0aca879a5673469f62d4cbff403dbfdb37
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-02-04 20:52:08 +0530

    47 katas completed

--- 8kyu/rockPaperScissors.js
+++ 8kyu/rockPaperScissors.js
@@ -12,26 +12,40 @@
 const rockPaperScissors = (p1, p2) => {  
 
     // Method #1 : Solution by me
-    if(p1 === p2)  {
-        return 'Draw!';
-    } else if(p1 === 'rock'){
-        if(p2 === 'scissors'){
-            return 'Player 1 won!';
-        } else {
-            return 'Player 2 won!';
-        }
-    } else if(p1 === 'scissors'){
-        if(p2 === 'paper'){
-            return 'Player 1 won!';
-        } else {
-            return 'Player 2 won!';
-        }
-    } else if(p1 === 'paper')     {
-        if(p2 === 'rock'){
-            return 'Player 1 won!';
-        } else {
-            return 'Player 2 won!';
-        }
+    // if(p1 === p2)  {
+    //     return 'Draw!';
+    // } else if(p1 === 'rock'){
+    //     if(p2 === 'scissors'){
+    //         return 'Player 1 won!';
+    //     } else {
+    //         return 'Player 2 won!';
+    //     }
+    // } else if(p1 === 'scissors'){
+    //     if(p2 === 'paper'){
+    //         return 'Player 1 won!';
+    //     } else {
+    //         return 'Player 2 won!';
+    //     }
+    // } else if(p1 === 'paper')     {
+    //     if(p2 === 'rock'){
+    //         return 'Player 1 won!';
+    //     } else {
+    //         return 'Player 2 won!';
+    //     }
+    // }
+
+    // Method #2 : Solution found on Codewars
+    if(p1 === p2){
+        return 'Draw!'
+    } 
+    if(p1 === 'rock' && p2 === 'scissors'){
+        return 'Player 1 won!';
+    } else if(p1 === 'paper' && p2 === 'rock'){
+        return 'Player 1 won!';
+    } else if(p1 === 'scissors' && p2 === 'paper'){
+        return 'Player 1 won!';
+    } else {
+        return 'Player 2 won!';
     }
 }
 
