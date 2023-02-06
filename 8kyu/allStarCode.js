ffa4c8b9fab94c03c64039c3efe7bbae66c70d7b
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-02-06 19:03:06 +0530

    50 katas completed

---
+++ 8kyu/allStarCode.js
@@ -0,0 +1,19 @@
+// Kata # 50 of 8kyu
+// All Star Code Challenge #18
+// Create a function that accepts 2 string arguments and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+// If no occurrences can be found, a count of 0 should be returned.
+// ("Hello", "o")  ==>  1
+// ("Hello", "l")  ==>  2
+// ("", "z")       ==>  0
+// Notes:
+//     The first argument can be an empty string
+//     The second string argument will always be of length 1
+
+const allStarCode = (string1, string2) => {
+    // Method #1 : Solution by me
+    return string1.split('').filter(item=>item === string2).length;
+}
+
+console.log(allStarCode("Hello", "o"));
+console.log(allStarCode("Hello", "l"));
+console.log(allStarCode("", "z"));
