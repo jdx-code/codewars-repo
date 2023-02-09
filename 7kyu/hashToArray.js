18030abcf54fa36e9bb01f1eea45126e0eddfbac
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-02-09 13:05:48 +0530

    34 katas completed

---
+++ 7kyu/hashToArray.js
@@ -0,0 +1,20 @@
+// Kata # 34 of 7kyu
+// Description:
+
+// Convert a hash into an array. Nothing more, Nothing less.
+
+// {name: 'Jeremy', age: 24, role: 'Software Engineer'}
+
+// should be converted into
+
+// [["age", 24], ["name", "Jeremy"], ["role", "Software Engineer"]]
+
+// Note: The output array should be sorted alphabetically by key name.
+
+// Good Luck!
+
+const hashToArray = (obj) => {
+    return Object.entries(obj).sort();
+}
+
+console.log(hashToArray({name: 'Jeremy', age: 24, role: 'Software Engineer'}));
