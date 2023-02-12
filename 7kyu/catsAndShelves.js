a26b1b461dc1eb263c90d30dd28ac3842bc611dd
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-02-12 20:37:35 +0530

    37 katas completed

--- 7kyu/catsAndShelves.js
+++ 7kyu/catsAndShelves.js
@@ -1,3 +1,4 @@
+// Kata # 37 of 7kyu
 // Description
 // An infinite number of shelves are arranged one above the other in a staggered fashion.
 // The cat can jump either one or three shelves at a time: from shelf i to shelf i+1 or i+3 (the cat cannot climb on the shelf directly above its head), according to the illustration:
@@ -26,11 +27,20 @@
 // Example
 // Start 1, finish 5, then answer is 2 (1 => 4 => 5 or 1 => 2 => 5)
 
-const catsAndShelves = (start, finish) => {
-    let totalSteps = finish - start;
-    let remainingSteps = totalSteps % 3;
-    let longSteps = (totalSteps - remainingSteps) / 3;
-    return longSteps + remainingSteps;
+
+// Method #1 : Solution found on Codewars
+// ``````````````````````````````````````
+// const catsAndShelves = (start, finish) => {
+//     // let totalSteps = finish - start;
+//     // let remainingSteps = totalSteps % 3;
+//     // let longSteps = (totalSteps - remainingSteps) / 3;
+//     // return longSteps + remainingSteps;
+// }
+
+// Method #1 : Solution found on Codewars
+// ``````````````````````````````````````
+const catsAndShelves = (start, finish, difference = finish - start) => {
+    return Math.floor(difference / 3) + (difference % 3);
 }
 
 console.log(catsAndShelves(1, 5));
