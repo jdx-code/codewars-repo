e132060e6a5e58dc961ee226487ab4eb2327a8b3
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-02-16 22:05:17 +0530

    54 katas completed

---
+++ 8kyu/argumentSquare.js
@@ -0,0 +1,8 @@
+// Kata # 54 of 8kyu
+// Now you have to write a function that takes an argument and returns the square of it.
+
+const argumentSquare = (num) => {
+    return Math.pow(num, 2);
+}
+
+console.log(argumentSquare(3));
