4db5311d46cbd57f4fb27fd29bd57aefd8ab670f
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-02-17 20:30:40 +0530

    55 katas completed

--- 8kyu/saleHotDogs.js
+++ 8kyu/saleHotDogs.js
@@ -8,6 +8,12 @@
 // You can use if..else or ternary operator to complete it.
 
 const saleHotDogs = (n) => {
-    return n < 5 ? n * 100 : (n < 10 ? n * 95 : n * 90) ;
+    // Method #1 : Using ternary operator
+    // return n < 5 ? n * 100 : (n < 10 ? n * 95 : n * 90) ;
+
+    // Method #2 : Using if-else
+    if(n < 5) return n * 100;
+    else if(n > 5 && n < 10) return n * 95;
+    else return n * 90;
 }
 console.log(saleHotDogs(4));
