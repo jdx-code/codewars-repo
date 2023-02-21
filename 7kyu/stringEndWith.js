5547ea0afce25b109de75777d9a691fbe98b8590
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-02-21 11:22:30 +0530

    43 katas completed

---
+++ 7kyu/stringEndWith.js
@@ -0,0 +1,20 @@
+// Kata # 43 of 7kyu
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+const stringEndWith = (str1, str2) => {
+    // Method #1 : My Solution
+    const lenghtOfStr2 = str2.length;
+    if(lenghtOfStr2){
+        return str1.slice(-lenghtOfStr2) === str2;
+    } else {
+        return true;
+    }        
+}
+
+console.log(stringEndWith('abc', 'bc'));
+console.log(stringEndWith('abc', 'd'));
