18527f3cef7d754920d0d38b83a2d14f8b17db1a
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-02-21 11:25:00 +0530

    43 katas completed

--- 7kyu/stringEndWith.js
+++ 7kyu/stringEndWith.js
@@ -8,12 +8,15 @@
 
 const stringEndWith = (str1, str2) => {
     // Method #1 : My Solution
-    const lenghtOfStr2 = str2.length;
-    if(lenghtOfStr2){
-        return str1.slice(-lenghtOfStr2) === str2;
-    } else {
-        return true;
-    }        
+    // const lenghtOfStr2 = str2.length;
+    // if(lenghtOfStr2){
+    //     return str1.slice(-lenghtOfStr2) === str2;
+    // } else {
+    //     return true;
+    // }
+    
+    // Method #2 : Solution found on Codewars
+    return str1.endsWith(str2);
 }
 
 console.log(stringEndWith('abc', 'bc'));
