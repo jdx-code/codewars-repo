b376d539d9591980f96e723df3b5fa56925848ed
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-02-24 12:18:41 +0530

    47 katas completed

---
+++ 7kyu/findingMiddle.js
@@ -0,0 +1,20 @@
+// Kata # 47 of 7kyu
+// As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+// The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+// For example:
+// gimme([2, 3, 1]) => 0
+// 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+// Another example (just to make sure it is clear):
+// gimme([5, 10, 14]) => 1
+// 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
+// Method #1 : Using sort( ) and indexOf( )
+const findingMiddle = (arr) => {
+    let newArr = [...arr];
+    const midElement = newArr.sort((a, b) => +a - +b)[1];
+    return arr.indexOf(midElement);
+}
+
+console.log(findingMiddle([2, 3, 1]));
+console.log(findingMiddle([5, 10, 14]));
+console.log(findingMiddle([2.1, 3.2, 1.4]));
