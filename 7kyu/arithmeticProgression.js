70ee1b45c214e51c5b9d62688f76c6bb3c45174b
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-03-01 22:23:55 +0530

    50 katas completed

---
+++ 7kyu/arithmeticProgression.js
@@ -0,0 +1,21 @@
+// Kata # 50 of 7kyu
+// In your class, you have started lessons about arithmetic progression. Since you are also a programmer, you have decided to write a function that will return the first n elements of the sequence with the given common difference d and first element a. Note that the difference may be zero!
+// The result should be a string of numbers, separated by comma and space.
+// Example
+// # first element: 1, difference: 2, how many: 5
+// arithmetic_sequence_elements(1, 2, 5) == "1, 3, 5, 7, 9"
+
+const arithmeticProg = (a, d, n) => {
+    // Method #1 : Solution found on Codewars
+    let arr = [a];            
+    while(n-1){                 
+        arr.push(a+=d);       
+        --n;
+    }
+    return arr;
+}
+
+console.log(arithmeticProg(1, 2, 5));
+console.log(arithmeticProg(1, 0, 5));
+console.log(arithmeticProg(1, -3, 10));
+console.log(arithmeticProg(100, -10, 10)); 
