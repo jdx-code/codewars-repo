a71059f48ec7e033c2e4014f8413c63797541a2b
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-03-05 20:44:26 +0530

    working on kata 9

---
+++ 6kyu/consecutiveStrings.js
@@ -0,0 +1,33 @@
+// Kata # 9 of 6kyu
+// You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+// Examples:
+// strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+// Concatenate the consecutive strings of strarr by 2, we get:
+// treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+// folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+// trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+// blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+// abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+// Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+// The first that came is "folingtrashy" so 
+// longest_consec(strarr, 2) should return "folingtrashy".
+// In the same way:
+// longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+// n being the length of the string array, if n = 0 or k > n or k <= 0 return "" (return Nothing in Elm, "nothing" in Erlang).
+// Note
+// consecutive strings : follow one after another without an interruption
+
+const longestConsecutiveStr = ( strarr, k) => {
+
+    return strarr.map((ele, index) => {
+        if(index != strarr.length)
+        return k > 1 ? (ele + strarr[index+(k-1)]) : ele;
+    }).sort((a,b) => (b.length - a.length))
+
+}
+
+console.log(longestConsecutiveStr(["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], 2));
+console.log(longestConsecutiveStr(["zone", "abigail", "theta", "form", "libe", "zas"], 2));
+console.log(longestConsecutiveStr(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1));
+
+
