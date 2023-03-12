510e4c5e52223891bd4052c31dee23533a93e2e1
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-03-12 22:41:59 +0530

    kata 10 added

---
+++ 6kyu/countCharactersInString.js
@@ -0,0 +1,3 @@
+// Kata # 10 of 6kyu
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+// What if the string is empty? Then the result should be empty object literal, {}.
