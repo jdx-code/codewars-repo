03a78d752a6d18bf9d5bbc642e12ffab3691ba99
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-03-19 21:47:50 +0530

    kata completed

---
+++ 6kyu/kata-11-findTheOddInt.js
@@ -0,0 +1,20 @@
+// Kata # 11 of 6kyu
+// Given an array of integers, find the one that appears an odd number of times.
+// There will always be only one integer that appears an odd number of times.
+// Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+const findTheOddInt = (arr) => {
+    // Method #1 : Solution found on Codewars
+    return arr.reduce((a,b) => a ^ b);
+}
+
+console.log(findTheOddInt([7]));
+console.log(findTheOddInt([0]));
+console.log(findTheOddInt([1,1,2]));
+console.log(findTheOddInt([0,1,0,1,0]));
+console.log(findTheOddInt([1,2,2,3,3,3,4,3,3,3,2,2,1]));
