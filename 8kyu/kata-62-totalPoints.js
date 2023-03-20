d8fa574555e8a6d4874e4c2ea2055da89b0894e4
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-03-20 16:38:36 +0530

    62 katas completed

--- 8kyu/kata-62-totalPoints.js
+++ 8kyu/kata-62-totalPoints.js
@@ -12,17 +12,22 @@
 //     0 <= y <= 4
 
 // Method #1 : Solution found on Codewars
-function totalPoints(games) {
-    let sum=0;
-    for (let i=0; i<games.length; ++i)
-    {
-      if (games[i][0]>games[i][2])
-        sum+=3;
-      if (games[i][0]==games[i][2])
-        sum+=1;
-    }
-    return sum;
-  }
+// function totalPoints(games) {
+//     let sum=0;
+//     for (let i=0; i<games.length; ++i)
+//     {
+//       if (games[i][0]>games[i][2])
+//         sum+=3;
+//       if (games[i][0]==games[i][2])
+//         sum+=1;
+//     }
+//     return sum;
+//   }
+
+// Method #2 : Solution found on Codewars
+const totalPoints = (arr) => {
+    return arr.reduce((a, [x, _, y]) => a + (x > y ? 3 : x == y), 0)
+}
 
 console.log(totalPoints(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]));
 console.log(totalPoints(["1:1","2:2","3:3","4:4","2:2","3:3","4:4","3:3","4:4","4:4"]));
