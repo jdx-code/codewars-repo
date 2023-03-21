45a33915985204435f25df2e9497c8e122811456
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-03-21 12:48:29 +0530

    kata 13

--- 6kyu/kata-13-duplicateEncoder.js
+++ 6kyu/kata-13-duplicateEncoder.js
@@ -1,3 +1,4 @@
+// Kata # 13 of 6kyu
 // The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
 // Examples
 // "din"      =>  "((("
@@ -6,3 +7,5 @@
 // "(( @"     =>  "))((" 
 // Notes
 // Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
+
