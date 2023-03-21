3e071101b5e1ed90f1f80d66d631f893b1d2c520
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-03-21 12:44:41 +0530

    62 katas completed

--- 8kyu/kata-62-totalPoints.js
+++ 8kyu/kata-62-totalPoints.js
@@ -26,7 +26,7 @@
 
 // Method #2 : Solution found on Codewars
 const totalPoints = (arr) => {
-    return arr.reduce((a, [x, _, y]) => a + (x > y ? 3 : x == y), 0)
+    return arr.reduce((a, [x, _, y]) => a + (x > y ? 3 : x == y), 0)    // the last x == y can also be written as x == y ? 1 : 0
 }
 
 console.log(totalPoints(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]));
