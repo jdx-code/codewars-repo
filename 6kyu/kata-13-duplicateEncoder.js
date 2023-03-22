46028d1dfec4f913c4f88f943d42c70cc90e2485
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-03-22 12:54:25 +0530

    13 katas completed

--- 6kyu/kata-13-duplicateEncoder.js
+++ 6kyu/kata-13-duplicateEncoder.js
@@ -8,4 +8,19 @@
 // Notes
 // Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
 
+// Method #1: Solution from Codewars
 
+const duplicateEncoder = (str) => {
+    return str
+        .toLowerCase()
+        .split('')
+        .map((e, i, str) => {
+            return str.indexOf(e) == str.lastIndexOf(e) ? '(' : ')'
+        }) 
+        .join('');
+}
+
+console.log(duplicateEncoder("din"));
+console.log(duplicateEncoder("recede"));
+console.log(duplicateEncoder("Success"));
+console.log(duplicateEncoder("(( @"));
