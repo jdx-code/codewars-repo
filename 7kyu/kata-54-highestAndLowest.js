f174826d263888104ef6474729c2061230e8615d
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-04-01 21:51:36 +0530

    kata added

--- 7kyu/kata-54-highestAndLowest.js
+++ 7kyu/kata-54-highestAndLowest.js
@@ -1,4 +1,3 @@
-// Kata # 22 of 7kyu
 // In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
 // Examples
 // highAndLow("1 2 3 4 5");  // return "5 1"
