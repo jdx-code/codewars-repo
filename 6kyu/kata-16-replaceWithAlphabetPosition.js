4aaa578cec1dfd8b076ecd1b9a85befa7d2b136b
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-04-02 20:20:34 +0530

    16 katas completed

--- 6kyu/kata-16-replaceWithAlphabetPosition.js
+++ 6kyu/kata-16-replaceWithAlphabetPosition.js
@@ -7,7 +7,49 @@
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
 
 const replaceWithAlphabetPosition = (str) => {
-    return str;
+
+    // Method #1 : Solution by me
+    
+    const strLowerCase = str.toLowerCase();
+
+    const alphabetToNum = {
+        'a' : 1,
+        'b' : 2,
+        'c' : 3,
+        'd' : 4,
+        'e' : 5,
+        'f' : 6,
+        'g' : 7,
+        'h' : 8,
+        'i' : 9,
+        'j' : 10,
+        'k' : 11,
+        'l' : 12,
+        'm' : 13,
+        'n' : 14,
+        'o' : 15,
+        'p' : 16,
+        'q' : 17,
+        'r' : 18,
+        's' : 19,
+        't' : 20,
+        'u' : 21,
+        'v' : 22,
+        'w' : 23,
+        'x' : 24,
+        'y' : 25,
+        'z' : 26,
+    };
+
+    const newArr = [];
+
+    for(s of strLowerCase){        
+        if(s in alphabetToNum){
+            newArr.push(alphabetToNum[s]);    
+        }        
+    }
+
+    return newArr.join(' ');
 }
 
 console.log(replaceWithAlphabetPosition("The sunset sets at twelve o' clock."))
