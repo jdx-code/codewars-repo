b32a32d32d38b54c29b646e86684cede4e3ac18a
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-04-02 20:44:46 +0530

    16 katas completed

--- 6kyu/kata-16-replaceWithAlphabetPosition.js
+++ 6kyu/kata-16-replaceWithAlphabetPosition.js
@@ -10,46 +10,58 @@ const replaceWithAlphabetPosition = (str) => {
 
     // Method #1 : Solution by me
     
-    const strLowerCase = str.toLowerCase();
+    // const strLowerCase = str.toLowerCase();
 
-    const alphabetToNum = {
-        'a' : 1,
-        'b' : 2,
-        'c' : 3,
-        'd' : 4,
-        'e' : 5,
-        'f' : 6,
-        'g' : 7,
-        'h' : 8,
-        'i' : 9,
-        'j' : 10,
-        'k' : 11,
-        'l' : 12,
-        'm' : 13,
-        'n' : 14,
-        'o' : 15,
-        'p' : 16,
-        'q' : 17,
-        'r' : 18,
-        's' : 19,
-        't' : 20,
-        'u' : 21,
-        'v' : 22,
-        'w' : 23,
-        'x' : 24,
-        'y' : 25,
-        'z' : 26,
-    };
+    // const alphabetToNum = {
+    //     'a' : 1,
+    //     'b' : 2,
+    //     'c' : 3,
+    //     'd' : 4,
+    //     'e' : 5,
+    //     'f' : 6,
+    //     'g' : 7,
+    //     'h' : 8,
+    //     'i' : 9,
+    //     'j' : 10,
+    //     'k' : 11,
+    //     'l' : 12,
+    //     'm' : 13,
+    //     'n' : 14,
+    //     'o' : 15,
+    //     'p' : 16,
+    //     'q' : 17,
+    //     'r' : 18,
+    //     's' : 19,
+    //     't' : 20,
+    //     'u' : 21,
+    //     'v' : 22,
+    //     'w' : 23,
+    //     'x' : 24,
+    //     'y' : 25,
+    //     'z' : 26,
+    // };
 
-    const newArr = [];
+    // const newArr = [];
 
-    for(s of strLowerCase){        
-        if(s in alphabetToNum){
-            newArr.push(alphabetToNum[s]);    
-        }        
-    }
+    // for(s of strLowerCase){        
+    //     if(s in alphabetToNum){
+    //         newArr.push(alphabetToNum[s]);    
+    //     }        
+    // }
 
-    return newArr.join(' ');
+    // return newArr.join(' ');
+
+
+    // Method #2 : Solution idea from Codewars
+
+    const strInLowerCase = str.toLowerCase();
+    
+    const alphabets = "abcdefghijklmnopqrstuvwxyz";
+
+    return strInLowerCase.split('').filter(s=>{
+        let index = alphabets.indexOf(s);
+        return index > -1;                  // means only those which exists
+    }).map(s=>alphabets.indexOf(s)+1).join(' ');
 }
 
 console.log(replaceWithAlphabetPosition("The sunset sets at twelve o' clock."))
