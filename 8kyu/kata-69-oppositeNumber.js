ed5d92aea1c0b2d4aa6fbe8a3e5983b920a4d0c6
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-04-15 19:51:22 +0530

    69 katas completed

---
+++ 8kyu/kata-69-oppositeNumber.js
@@ -0,0 +1,15 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+const oppositeNumber = (num) => {
+    return -(num);
+}
+
+console.log(oppositeNumber(1))
+console.log(oppositeNumber(14))
+console.log(oppositeNumber(-34))
