dfe28e918b3164aa85cc7249e64d1d6f2fbd726e
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-04-20 22:45:03 +0530

    64 katas completed

---
+++ 7kyu/kata-64-keepTheOrder.js
@@ -0,0 +1,37 @@
+// Task:
+// Your job here is to write a function (keepOrder in JS/CoffeeScript, keep_order in Ruby/Crystal/Python, keeporder in Julia), which takes a sorted array ary and a value val, and returns the lowest index where you could insert val to maintain the sorted-ness of the array. The input array will always be sorted in ascending order. It may contain duplicates.
+// Do not modify the input.
+// Some examples:
+// keepOrder([1, 2, 3, 4, 7], 5) //=> 4
+//                       ^(index 4)
+// keepOrder([1, 2, 3, 4, 7], 0) //=> 0
+//           ^(index 0)
+// keepOrder([1, 1, 2, 2, 2], 2) //=> 2
+//                 ^(index 2)
+
+
+const keepOrder = (ary, val) => {
+    let index = 0;
+    for(let i=0; i<ary.length; i++){
+        if(val<=ary[i]){
+            index = i;
+            break;
+        }
+    }
+    if(val>ary[ary.length-1]){
+        index = ary.length;
+    }
+    return index;
+}
+
+console.log(keepOrder([1, 2, 3, 4, 7], 5))
+console.log(keepOrder([1, 2, 3, 4, 7], 0))
+console.log(keepOrder([1, 1, 2, 2, 2], 2))
+console.log(keepOrder([1, 2, 3, 4], 5))
+console.log(keepOrder([1, 2, 3, 4], -1))
+console.log(keepOrder([1, 2, 3, 4], 2))
+console.log(keepOrder([1, 2, 3, 4], 0))
+console.log(keepOrder([1, 2, 3, 4], 1))
+console.log(keepOrder([1, 2, 3, 4], 2))
+console.log(keepOrder([1, 2, 3, 4], 3))
+console.log(keepOrder([-5, -4, -2, -1, 1, 2], -2))
