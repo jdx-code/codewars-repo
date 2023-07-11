f3ad2a01d351d52543fc691681963ad8a54d430b
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-07-11 15:37:13 +0530

    31 katas completed

--- 6kyu/kata-31-buildTower.js
+++ 6kyu/kata-31-buildTower.js
@@ -16,13 +16,19 @@
 // ]
 
 const buildTower = (x) => {
-
-    let newArr = []
-    for(let i=0; i<x; i++){
-        newArr.push(i+(i+1))
-    }
+    // My solution
+    // let newArr = []
+    // for(let i=0; i<x; i++){
+    //     newArr.push(i+(i+1))
+    // }
     
-    return newArr.map((e, i) => ' '.repeat((newArr.length-1)-i) + '*'.repeat(e) + ' '.repeat((newArr.length-1)-i))
+    // return newArr.map((e, i) => ' '.repeat((newArr.length-1)-i) + '*'.repeat(e) + ' '.repeat((newArr.length-1)-i))
+    
+    // Codewars solution (more efficient)
+    return [...Array(x)].map((_,i) => {
+        const spaces = ' '.repeat(x-i-1) 
+        return spaces + '*'.repeat(i+i+1) + spaces
+    })
 }
 
 console.log(buildTower(3))
