0871a64eda6ebcb4bda7f4efa44eac9bd4404a5b
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-08-12 15:36:59 +0530

    77 katas completed

---
+++ 8kyu/kata-77-reversedWords.js
@@ -0,0 +1,13 @@
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+// Words are separated by exactly one space and there are no leading or trailing spaces.
+
+// Example(Input --> Output):
+
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+const reversedWords = (str) => {
+    return str.split(' ').reverse().join(' ')
+}
+
+console.log(reversedWords("The greatest victory is that which requires no battle"))
