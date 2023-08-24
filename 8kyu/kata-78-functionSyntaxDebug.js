b4fea7845c9094b843febe32fd9bc36c986d3e95
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-08-24 16:51:43 +0530

    78 katas completed

---
+++ 8kyu/kata-78-functionSyntaxDebug.js
@@ -0,0 +1,9 @@
+// Grasshopper - Function syntax debugging
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+const syntaxDebug = (verb, noun) => {
+    return `${verb} ${noun}`
+}
+
+console.log(syntaxDebug('take', 'item'))
+console.log(syntaxDebug('use', 'sword'))
